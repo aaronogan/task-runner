ac69425a8bbf8c6b0ed64164b241f05d1cbde0e2
Aaron Ogan <aaron@aaronogan.com>
2013-05-13 18:27:24 -0500

    First go at stub persistence

---
+++ TaskRunner.DataAccess/JobPersistence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskRunner.Tasks;
+
+namespace TaskRunner.DataAccess
+{
+    public interface JobPersistence
+    {
+        void Save(JobResult result);
+    }
+}
---
+++ TaskRunner.DataAccess/JobPersistenceStub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskRunner.Tasks;
+
+namespace TaskRunner.DataAccess
+{
+    public class JobPersistenceStub : JobPersistence
+    {
+        public void Save(JobResult result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
