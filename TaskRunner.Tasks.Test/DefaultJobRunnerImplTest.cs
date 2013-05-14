1d0595ee2ee278b9e02eb259b80ceaceebcc72c8
Aaron Ogan <aaron@aaronogan.com>
2013-05-13 21:20:05 -0500

    Setting up for JobRunner

---
+++ TaskRunner.Tasks.Test/DefaultJobRunnerImplTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskRunner.Tasks;
+
+namespace TaskRunner.Tasks.Test
+{
+    [TestClass]
+    public class DefaultJobRunnerImplTest
+    {
+    }
+}
---
+++ TaskRunner.Tasks/JobRunner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskRunner.Tasks
+{
+    public interface JobRunner
+    {
+        void Execute();
+    }
+
+    public class DefaultJobRunnerImpl : JobRunner
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
