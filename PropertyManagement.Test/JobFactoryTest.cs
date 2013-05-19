d95c87633d651f7c01877d5589c48bf6466b5ca2
Aaron Ogan <aaron@aaronogan.com>
2013-05-19 13:10:39 -0500

    Adding test project for PropertyManagement example

---
+++ PropertyManagement.Test/JobFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskRunner.Tasks;
+using PropertyManagement;
+
+namespace PropertyManagement.Test
+{
+    [TestClass]
+    public class JobFactoryTest
+    {
+        [TestMethod]
+        public void GetJob_Returns_Proper_Implementation_For_Rent_Charges()
+        {
+            var job = JobFactory.GetJob(1, "Rent Charges", 20, null);
+
+            Assert.IsInstanceOfType(job, typeof(RentChargesJob));
+        }
+    }
+}
---
+++ PropertyManagement/JobFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskRunner.Tasks;
+
+namespace PropertyManagement
+{
+    public class JobFactory
+    {
+        public static Job GetJob(int id, string name, int maxDurationSeconds, int? dependencyId)
+        {
+            switch (name.ToLower())
+            {
+                case "rent charges":
+                    return new RentChargesJob(id, name, maxDurationSeconds, dependencyId);
+                default:
+                    return new DefaultJob(id, name, maxDurationSeconds, dependencyId);
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
